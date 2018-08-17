743508dad89a8923af3383ad1cc702ee8cc583cd
Jeroen Peeters <jeroen.peeters@evance.be>
2018-08-17 22:20:39 +0200

    chore(app): remove access token

--- src/networking/consts.js
+++ src/networking/consts.js
@@ -4,5 +4,4 @@ export const REPOS = "/repos";
 export const COMMITS = "/commits";
 export const PAGE = "&page=";
 export const SLASH = "/";
-export const ACCESS_TOKEN =
-  "?access_token=7625c2789a51192a5d6a4c26e14c6ad3c3e060a0";
+export const ACCESS_TOKEN = "?access_token=";
