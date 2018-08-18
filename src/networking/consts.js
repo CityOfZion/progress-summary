c39448b5b64616248b35e24152c14d7e8c021000
Jeroen Peeters <jeroen.peeters@evance.be>
2018-08-18 10:55:35 +0200

    chore(app): potentially fix github deploys

--- src/networking/consts.js
+++ src/networking/consts.js
@@ -1,7 +1,10 @@
+import accessToken from "./accessToken";
+import parseToken from "./parseToken";
+
 export const GITHUB_API = "https://api.github.com";
 export const ORGS = "/orgs";
 export const REPOS = "/repos";
 export const COMMITS = "/commits";
 export const PAGE = "&page=";
 export const SLASH = "/";
-export const ACCESS_TOKEN = "?access_token=";
+export const ACCESS_TOKEN = `?access_token=${parseToken(accessToken)}`;
